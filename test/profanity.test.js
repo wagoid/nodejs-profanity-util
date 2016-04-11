74c0c563e936fd5ef04a824d1c62b879e3ab5eea
wagoid <w.rsantos@outlook.com>
2016-04-11 02:23:42 -0300

    Bug fixes and test additions.

--- test/profanity.test.js
+++ test/profanity.test.js
@@ -5,69 +5,188 @@ License: http://opensource.org/licenses/MIT The MIT License (MIT)
 
 var should = require('should'),
     util = require('./util'),
-    profanity = require('../lib/profanity');
+    profanity = require('../lib/profanity'),
+    _ = require('underscore');
+
+function callTestCheckFunctions(languages, forbiddenList) {
+  it('Should return null with no swearwords found in string', function () {
+      should(profanity.check('No swearwords here', languages, forbiddenList)).eql([]);
+  });
+
+  it('Should retur array of swearwords found in dirty string', function () {
+      var results = profanity.check('something damn something something poo something', languages, forbiddenList);
+
+      should(results).eql([
+          'damn',
+          'poo'
+      ]);
+  });
+
+  it('Should not target substrings', function () {
+      var detected = profanity.check('foo ass bar', languages, forbiddenList),
+          notDetected = profanity.check('foo grass bar', languages, forbiddenList);
+
+      should(detected).have.length(1);
+      should(notDetected).have.length(0);
+  });
+
+  it('Should work equally for objects (Recursively) and arrays', function (done) {
+      var results_obj = profanity.check({
+              foo: 'something damn',
+              bar: { test: 'something poo', bar: 'crap woooh' }
+          }, languages, forbiddenList),
+          results_arr = profanity.check([
+              'something damn',
+              [ 'something poo' ],
+              { foo: [ { bar: 'something crap' } ] }
+          ], languages, forbiddenList);
+
+      should(results_obj).eql([
+          'damn',
+          'poo',
+          'crap'
+      ]);
+
+      should(results_arr).eql([
+          'damn',
+          'poo',
+          'crap'
+      ]);
+
+      done();
+  });
+}
 
 describe('Profanity module', function () {
-    describe('.validate(target)', function () {
- 
-        it('returns null with no swearwords found in string', function () {
-            should(profanity.check('No swearwords here')).eql([]);
+    describe('.check(target)', function () {
+       callTestCheckFunctions();
+    });
+    
+    describe('.check(target) when languages="all"', function () {
+       callTestCheckFunctions('all', null);
+    });
+    
+    describe('.check(target) for portuguese', function () {
+      it('Should not get a portuguese dirty word when there is no language specifided (default is english)', function () {
+          var notDetected = profanity.check('viados não gostam de pepeca', null);
+
+          should(notDetected).have.length(0);
+      });
+      
+      it('Should get a dirty word and not get substrings', function () {
+          var detected = profanity.check('viado não gosta de pepeca', 'pt');
+          var notDetected = profanity.check('viados não gosta de pepecas', 'pt');
+
+          detected.should.eql(['viado', 'pepeca']);
+          notDetected.should.eql([]);
+      });
+      
+      it('Should work equally for objects (Recursively) and arrays', function (done) {
+        var results_obj = profanity.check({ foo: 'something perereca', bar: { test: 'something punheta', bar: 'paspalhão woooh' } }, 'pt'),
+        results_arr = profanity.check(['something porra', [ 'something precheca' ], { foo: [ { bar: 'something puta' } ] }], 'pt');
+
+        should(results_obj).eql([
+            'perereca',
+            'punheta',
+            'paspalhão'
+        ]);
+
+        should(results_arr).eql([
+            'porra',
+            'precheca',
+            'puta'
+        ]);
+
+        done();
+      });
+    });
+    
+    describe('.check(target) for multiple languages', function () {
+      it('Should get a dirty word and not get substrings', function () {
+          var detected = profanity.check('viado não gosta de pepeca fucker bocchinara reudig', ['pt', 'it', 'de', 'en']);
+          var notDetected = profanity.check('viados não gosta de pepecas fuckersras, bocchinarasws reudigs', ['pt', 'it', 'de', 'en']);
+
+          detected.should.eql(['viado', 'pepeca', 'fucker', 'bocchinara', 'reudig']);
+          notDetected.should.eql([]);
+      });
+      
+      it('Should work equally for objects (Recursively) and arrays', function (done) {
+        var results_obj = profanity.check({ foo: 'something perereca fucker  ', bar: { test: 'something punheta bocchinara', bar: 'paspalhão reudig woooh' } }, ['pt', 'it', 'de', 'en']),
+        results_arr = profanity.check(['something porra fucker', [ 'something precheca bocchinara ' ], { foo: [ { bar: 'something puta reudig' } ] }], 'all');
+
+        should(results_obj).eql([
+            'perereca',
+            'fucker',
+            'punheta',
+            'bocchinara',
+            'paspalhão',
+            'reudig'
+        ]);
+
+        should(results_arr).eql([
+            'porra',
+            'fucker',
+            'precheca',
+            'bocchinara',
+            'puta',
+            'reudig'
+        ]);
+
+        done();
+      });
+    });
+    
+    describe('.checkAsync(target)', () => {
+      it('Returns null with no swearwords found in string', () => {
+        profanity.checkAsync('No swearwords here')
+          .then(results => {
+          should(results).eql([]);
         });
+      });
 
-        it('returns array of swearwords found in dirty string', function () {
-            var results = profanity.check('something damn something something poo something');
-
-            should(results).eql([
+      it('Returns array of swearwords found in dirty string', () => {
+          profanity.checkAsync('something damn something something poo something')
+            .then(results => {
+              should(results).eql([
                 'damn',
                 'poo'
-            ]);
-        });
+              ]);
+          });
+      });
 
-        it('doesn\'t target substrings', function () {
-            var detected = profanity.check('foo ass bar'),
-                notDetected = profanity.check('foo grass bar');
+      it('Doesn\'t target substrings', function () {
+        Promise.all([profanity.checkAsync('foo ass bar'), profanity.checkAsync('foo grass bar')])
+          .then( results => {
+            var detected = results[0];
+            var notDetected = results[1];
+            
+            detected.should.have.length(1);
+            notDetected.should.have.length(0);
+          });
+      });
 
-            should(detected).have.length(1);
-            should(notDetected).have.length(0);
-        });
-
-        it('works equally for objects (Recursively) and arrays', function (done) {
-            var results_obj = profanity.check({
-                    foo: 'something damn',
-                    bar: { test: 'something poo', bar: 'crap woooh' }
-                }),
-                results_arr = profanity.check([
-                    'something damn',
-                    [ 'something poo' ],
-                    { foo: [ { bar: 'something crap' } ] }
-                ]);
-
-            should(results_obj).eql([
-                'damn',
-                'poo',
-                'crap'
-            ]);
-
-            should(results_arr).eql([
-                'damn',
-                'poo',
-                'crap'
-            ]);
-
-            done();
-        });
-
+      it('Works equally for objects (Recursively) and arrays', function () {
+        var firstTarget = { foo: 'something damn', bar: { test: 'something poo', bar: 'crap woooh' } };
+        var secondTarget = { foo: 'something damn', bar: { test: 'something poo', bar: 'crap woooh' } };
+        var thirdTarget = ['something damn', [ 'something poo' ], { foo: [ { bar: 'something crap' } ] }];
+        Promise.all([profanity.checkAsync(firstTarget), profanity.checkAsync(secondTarget), profanity.checkAsync(thirdTarget)])
+          .then(results => {
+            results.should.have.length(3);
+            _.each(results, result => {
+              result.should.eql(['damn','poo','crap']);
+            });
+          });
+      });
     });
+    
+    
     describe('.purify(target)', function () {
 
-        it('works in obscure (default) mode on a simple string', function (done) {
+        it('works in obscure (default) mode on a simple string', function () {
             var result = profanity.purify('boob damn something poo');
 
             result[0].should.equal('b**b d**n something p*o');
             result[1].should.eql([ 'boob', 'damn', 'poo' ]);
-
-
-            done();
         });
 
         it('works in obscure (default) mode recursively with objects', function (done) {
