911d011192f57aebf968722e8a3bc20001e388a4
wagoid <w.rsantos@outlook.com>
2016-04-13 02:53:30 -0300

    Add purifyAsync, made some changes to util and made 100% coverage

--- test/profanity.test.js
+++ test/profanity.test.js
@@ -13,7 +13,7 @@ function callTestCheckFunctions(languages, forbiddenList) {
       should(profanity.check('No swearwords here', languages, forbiddenList)).eql([]);
   });
 
-  it('Should retur array of swearwords found in dirty string', function () {
+  it('Should return array of swearwords found in dirty string', function () {
       var results = profanity.check('something damn something something poo something', languages, forbiddenList);
 
       should(results).eql([
@@ -30,7 +30,7 @@ function callTestCheckFunctions(languages, forbiddenList) {
       should(notDetected).have.length(0);
   });
 
-  it('Should work equally for objects (Recursively) and arrays', function (done) {
+  it('Should work equally for objects (Recursively) and arrays', function () {
       var results_obj = profanity.check({
               foo: 'something damn',
               bar: { test: 'something poo', bar: 'crap woooh' }
@@ -52,8 +52,6 @@ function callTestCheckFunctions(languages, forbiddenList) {
           'poo',
           'crap'
       ]);
-
-      done();
   });
 }
 
@@ -81,7 +79,7 @@ describe('Profanity module', function () {
           notDetected.should.eql([]);
       });
       
-      it('Should work equally for objects (Recursively) and arrays', function (done) {
+      it('Should work equally for objects (Recursively) and arrays', function () {
         var results_obj = profanity.check({ foo: 'something perereca', bar: { test: 'something punheta', bar: 'paspalhão woooh' } }, 'pt'),
         results_arr = profanity.check(['something porra', [ 'something precheca' ], { foo: [ { bar: 'something puta' } ] }], 'pt');
 
@@ -96,8 +94,6 @@ describe('Profanity module', function () {
             'precheca',
             'puta'
         ]);
-
-        done();
       });
     });
     
@@ -110,7 +106,7 @@ describe('Profanity module', function () {
           notDetected.should.eql([]);
       });
       
-      it('Should work equally for objects (Recursively) and arrays', function (done) {
+      it('Should work equally for objects (Recursively) and arrays', function () {
         var results_obj = profanity.check({ foo: 'something perereca fucker  ', bar: { test: 'something punheta bocchinara', bar: 'paspalhão reudig woooh' } }, ['pt', 'it', 'de', 'en']),
         results_arr = profanity.check(['something porra fucker', [ 'something precheca bocchinara ' ], { foo: [ { bar: 'something puta reudig' } ] }], 'all');
 
@@ -131,30 +127,46 @@ describe('Profanity module', function () {
             'puta',
             'reudig'
         ]);
-
-        done();
       });
     });
     
     describe('.checkAsync(target)', () => {
-      it('Returns null with no swearwords found in string', () => {
+      it('Should return null with no swearwords found in string', (done) => {
         profanity.checkAsync('No swearwords here')
           .then(results => {
-          should(results).eql([]);
-        });
+            should(results).eql([]);
+            done();
+          });
       });
 
-      it('Returns array of swearwords found in dirty string', () => {
+      it('Should return array of swearwords found in dirty string', (done) => {
           profanity.checkAsync('something damn something something poo something')
             .then(results => {
               should(results).eql([
                 'damn',
                 'poo'
               ]);
+              
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+            });
+      });
+      
+      it('Should work when passing my own forbidden list', (done) => {
+          profanity.checkAsync('something damn something something poo something', 'en', ['damn', 'poo'])
+            .then(results => {
+              should(results).eql([
+                'damn',
+                'poo'
+              ]);
+              
+              done();
           });
       });
 
-      it('Doesn\'t target substrings', function () {
+      it('Should not target substrings', function () {
         Promise.all([profanity.checkAsync('foo ass bar'), profanity.checkAsync('foo grass bar')])
           .then( results => {
             var detected = results[0];
@@ -165,7 +177,7 @@ describe('Profanity module', function () {
           });
       });
 
-      it('Works equally for objects (Recursively) and arrays', function () {
+      it('Should work equally for objects (Recursively) and arrays', function () {
         var firstTarget = { foo: 'something damn', bar: { test: 'something poo', bar: 'crap woooh' } };
         var secondTarget = { foo: 'something damn', bar: { test: 'something poo', bar: 'crap woooh' } };
         var thirdTarget = ['something damn', [ 'something poo' ], { foo: [ { bar: 'something crap' } ] }];
@@ -177,69 +189,140 @@ describe('Profanity module', function () {
             });
           });
       });
+      
+      it('Should jump to catch when passing language that is not supported by the tool', () => {
+        profanity.checkAsync('Anything', ['HUE'])
+          .catch(err => {
+            err.code.should.eql('ENOENT');
+          });
+      });
     });
     
     
     describe('.purify(target)', function () {
+      it('Should work in obscure (default) mode on a simple string', function () {
+        var result = profanity.purify('boob damn something poo');
 
-        it('works in obscure (default) mode on a simple string', function () {
-            var result = profanity.purify('boob damn something poo');
+        result[0].should.equal('b**b d**n something p*o');
+        result[1].should.eql([ 'boob', 'damn', 'poo' ]);
+      });
+      
+      it('Should work when passing my own forbidden list', function () {
+        var result = profanity.purify('boob damn something pota', { forbiddenList: ['boob', 'damn', 'pota'] });
 
-            result[0].should.equal('b**b d**n something p*o');
-            result[1].should.eql([ 'boob', 'damn', 'poo' ]);
+        result[0].should.equal('b**b d**n something p**a');
+        result[1].should.eql([ 'boob', 'damn', 'pota' ]);
+      });
+
+      it('Should work in obscure (default) mode recursively with objects, with infinite recursion and maxRecursionDepth', function () {
+        var testObj = {
+            bar: { foo: 'something boob', bar: { foo: 'test poo', bler: {foo: 'will not enter here'} } },
+            test: 'something damn'
+        };
+        testObj.crazy = testObj;
+        
+        var result = profanity.purify(testObj, {maxRecursionDepth: 1});
+
+        result[0].should.have.keys('bar', 'test', 'crazy');
+        result[0].bar.should.have.keys('foo', 'bar');
+        result[0].bar.foo.should.equal('something b**b');
+        result[0].bar.bar.should.have.keys('foo', 'bler');
+        result[0].bar.bar.foo.should.equal('test p*o');
+        result[0].bar.foo.should.equal('something b**b');
+        result[0].test.should.equal('something d**n');
+
+        result[1].should.eql([ 'boob', 'poo', 'damn' ]);
+      });
+
+      it('Should work in replace mode on a simple string', function () {
+        var result = profanity.purify('boob damn something poo', {
+            replace: true
         });
 
-        it('works in obscure (default) mode recursively with objects', function (done) {
-            var result = profanity.purify({
-                bar: { foo: 'something boob', bar: { foo: 'test poo' } },
-                test: 'something damn'
+        util.testPurified(result[0], '[ placeholder ] [ placeholder ] something [ placeholder ]');
+        result[1].should.eql([ 'boob', 'damn', 'poo' ]);
+      });
+
+      it('Should work in replace mode recursively with objects', function () {
+        var result = profanity.purify({
+            bar: { foo: 'something boob', bar: { foo: 'test poo' } },
+            test: 'something damn'
+        }, {
+            replace: true
+        });
+
+        result[0].should.have.keys('bar', 'test');
+        result[0].bar.should.have.keys('foo', 'bar');
+        util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
+        result[0].bar.bar.should.have.keys('foo');
+        util.testPurified(result[0].bar.bar.foo, 'test [ placeholder ]');
+        util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
+        util.testPurified(result[0].test, 'something [ placeholder ]');
+
+        result[1].should.eql([ 'boob', 'poo', 'damn' ]);
+      });
+    });
+    
+    describe('.purifyAsync(target)', function () {
+        it('Should work in obscure (default) mode on a simple string', function () {
+            profanity.purifyAsync('boob damn something poo')
+              .then(result => {
+                result[0].should.equal('b**b d**n something p*o');
+                result[1].should.eql([ 'boob', 'damn', 'poo' ]);
+              });
+        });
+
+        it('Should work in obscure (default) mode recursively with objects', function () {
+          var objToCheck = {
+            bar: { foo: 'something boob', bar: { foo: 'test poo' } },
+            test: 'something damn'
+          };
+          profanity.purifyAsync(objToCheck)
+            .then(result => {
+              result[0].should.have.keys('bar', 'test');
+              result[0].bar.should.have.keys('foo', 'bar');
+              result[0].bar.foo.should.equal('something b**b');
+              result[0].bar.bar.should.have.keys('foo');
+              result[0].bar.bar.foo.should.equal('test p*o');
+              result[0].bar.foo.should.equal('something b**b');
+              result[0].test.should.equal('something d**n');
+
+              result[1].should.eql([ 'boob', 'poo', 'damn' ]);
             });
-
-            result[0].should.have.keys('bar', 'test');
-            result[0].bar.should.have.keys('foo', 'bar');
-            result[0].bar.foo.should.equal('something b**b');
-            result[0].bar.bar.should.have.keys('foo');
-            result[0].bar.bar.foo.should.equal('test p*o');
-            result[0].bar.foo.should.equal('something b**b');
-            result[0].test.should.equal('something d**n');
-
-            result[1].should.eql([ 'boob', 'poo', 'damn' ]);
-
-            done();
         });
 
-        it('works in replace mode on a simple string', function (done) {
-            var result = profanity.purify('boob damn something poo', {
-                replace: true
+        it('Should work in replace mode on a simple string', function () {
+          profanity.purifyAsync('boob damn something poo', { replace: true })
+            .then(result => {
+              util.testPurified(result[0], '[ placeholder ] [ placeholder ] something [ placeholder ]');
+              result[1].should.eql([ 'boob', 'damn', 'poo' ]);
             });
-
-            util.testPurified(result[0], '[ placeholder ] [ placeholder ] something [ placeholder ]');
-            result[1].should.eql([ 'boob', 'damn', 'poo' ]);
-
-
-            done();
         });
 
-        it('works in replace mode recursively with objects', function (done) {
-            var result = profanity.purify({
-                bar: { foo: 'something boob', bar: { foo: 'test poo' } },
-                test: 'something damn'
-            }, {
-                replace: true
+        it('Should work in replace mode recursively with objects', function () {
+          var objToCheck = {
+            bar: { foo: 'something boob', bar: { foo: 'test poo' } },
+            test: 'something damn'
+          };
+          profanity.purifyAsync(objToCheck, { replace: true })
+            .then(result => {
+              result[0].should.have.keys('bar', 'test');
+              result[0].bar.should.have.keys('foo', 'bar');
+              util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
+              result[0].bar.bar.should.have.keys('foo');
+              util.testPurified(result[0].bar.bar.foo, 'test [ placeholder ]');
+              util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
+              util.testPurified(result[0].test, 'something [ placeholder ]');
+
+              result[1].should.eql([ 'boob', 'poo', 'damn' ]);
             });
-
-            result[0].should.have.keys('bar', 'test');
-            result[0].bar.should.have.keys('foo', 'bar');
-            util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
-            result[0].bar.bar.should.have.keys('foo');
-            util.testPurified(result[0].bar.bar.foo, 'test [ placeholder ]');
-            util.testPurified(result[0].bar.foo, 'something [ placeholder ]');
-            util.testPurified(result[0].test, 'something [ placeholder ]');
-
-            result[1].should.eql([ 'boob', 'poo', 'damn' ]);
-
-            done();
         });
-
+        
+        it('Should jump to catch when passing language that is not supported by the tool', () => {
+        profanity.purifyAsync('Anything', { languages: ['HUE'] })
+          .catch(err => {
+            err.code.should.eql('ENOENT');
+          });
+      });
     });
 });
